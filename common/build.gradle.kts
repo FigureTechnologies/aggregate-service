d420e3e1a43273049789dd18e89a74971106be3a
Stephen Cirner <scirner@figure.com>
2021-10-21 17:20:11 -0400

    Remove dupe versions.

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,36 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-object Version {
-    const val Arrow = "0.12.1"
-
-    object ApacheCommons {
-        const val CSV = "1.9.0"
-        const val IO = "2.11.0"
-        const val Lang3 = "3.12.0"
-        const val Text = "1.9"
-    }
-
-    const val AWS = "2.17.40"
-    const val DatadogStats = "2.13.0"
-    const val GRPC = "1.39.0"
-    const val Hoplite = "1.4.9"
-    const val JUnit = "5.1.0"
-    const val JUnitPioneer = "1.4.2"
-    const val JSON = "20210307"
-
-    object Kotlinx {
-        const val Core = "1.5.2"
-        const val CLI = "0.3.3"
-    }
-
-    const val Logback = "0.1.5"
-    const val Moshi = "1.12.0"
-    const val Provenance = "1.5.0"
-    const val Scarlet = "0.1.12"
-    const val AWSSDK = "2.17.32"
-    const val LocalStack = "0.2.15"
-}
-
 plugins {
     kotlin("jvm")
     id("org.jetbrains.kotlin.kapt")
