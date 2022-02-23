525db4670712511edeed96c3f98052f3e0774247
Robert Chaing <rchaing@figure.com>
2022-02-23 14:17:43 -0800

    fix unit test builder

--- src/test/kotlin/io/provenance/aggregate/service/utils/Builders.kt
+++ src/test/kotlin/io/provenance/aggregate/service/utils/Builders.kt
@@ -102,7 +102,7 @@ object Builders {
         var moshi: Moshi? = null
         var options: EventStream.Options.Builder = EventStream.Options.builder()
         var includeLiveBlocks: Boolean = true
-        var gasPriceUpdate: Pair<Double, Long>? = null
+        var feeCollector: String = ""
 
         fun <T : EventStreamService> eventStreamService(value: T) = apply { eventStreamService = value }
         fun <T : TendermintServiceClient> tendermintService(value: T) = apply { tendermintServiceClient = value }
@@ -111,7 +111,7 @@ object Builders {
         fun dispatchers(value: DispatcherProvider) = apply { dispatchers = value }
         fun options(value: EventStream.Options.Builder) = apply { options = value }
         fun includeLiveBlocks(value: Boolean) = apply { includeLiveBlocks = value }
-        fun gasPriceUpdate(value: Pair<Double, Long>) = apply { gasPriceUpdate = value }
+        fun feeCollector(value: String) = apply { feeCollector = value }
 
         // shortcuts for options:
         fun batchSize(value: Int) = apply { options.batchSize(value) }
@@ -135,6 +135,7 @@ object Builders {
                 dynamo = dynamoInterface ?: defaultAws().build().dynamo(),
                 moshi = moshi ?: Defaults.moshi,
                 dispatchers = dispatchers,
+                feeCollector = feeCollector,
                 options = options.build()
             )
         }
