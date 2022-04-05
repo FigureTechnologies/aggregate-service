e4ab4cf59e3bcdcb6250a898cb6042efe416a5d0
jhorecny-figure <jhorecny@figure.com>
2022-04-05 17:06:08 -0500

    use testnet node

--- src/main/kotlin/io/provenance/aggregate/service/Main.kt
+++ src/main/kotlin/io/provenance/aggregate/service/Main.kt
@@ -148,7 +148,7 @@ fun main(args: Array<String>) {
     val log = "main".logger()
 
     val decoderAdapter = moshiDecoderAdapter()
-    val netAdapter: NetAdapter = okHttpNetAdapter(host = config.eventStream.websocket.uri, tls = false)
+    val netAdapter: NetAdapter = okHttpNetAdapter(host = config.eventStream.websocket.uri, tls = true)
     val tendermintServiceClient = TendermintServiceOpenApiClient(config.eventStream.rpc.uri)
     val tendermintService = TendermintBlockFetcher(tendermintServiceClient)
     val aws: AwsClient = AwsClient.create(environment, config.aws.s3, config.aws.dynamodb)
