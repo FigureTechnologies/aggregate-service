2ec35190269afc0597692895177c4a0a269189b7
jhorecny-figure <jhorecny@figure.com>
2022-04-05 20:38:57 -0500

    isolate new env var for event stream refactor

--- common/src/main/kotlin/io/provenance/aggregate/common/Config.kt
+++ common/src/main/kotlin/io/provenance/aggregate/common/Config.kt
@@ -62,6 +62,7 @@ data class UploadConfig(
 
 data class Config (
     val aws: AwsConfig,
+    val wsNode: String,
     @ConfigAlias("event-stream") val eventStream: EventStreamConfig,
     val upload: UploadConfig = UploadConfig.empty(),
     val dbConfig: DBConfig,
