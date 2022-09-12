dd30acee96e55cb18357965902c6b8fc5cb4493e
Robert Chaing <rchaing@figure.com>
2022-09-12 13:34:31 -0700

    Add API that fetches total daily tx for given date range

--- service/src/main/kotlin/tech/figure/aggregator/api/service/AccountService.kt
+++ service/src/main/kotlin/tech/figure/aggregator/api/service/AccountService.kt
@@ -1,7 +1,12 @@
 package tech.figure.aggregator.api.service
 
+import com.papsign.ktor.openapigen.model.schema.DataFormat.date
 import tech.figure.aggregator.api.model.TxCoinTransferData
+import tech.figure.aggregator.api.model.TxDailyTotal
 import tech.figure.aggregator.api.model.TxFeeData
+import tech.figure.aggregator.api.route.toOffsetDateTime
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
 
 class AccountService{
 
@@ -37,6 +42,26 @@ class AccountService{
             }
         }
         return calcTotalAmt(feeAmtList)
+    }
+
+    fun organizeTxByDate(txCoinTransferDataList: List<TxCoinTransferData>, address: String): List<TxDailyTotal> {
+
+        // we only care for the 1st 10 characters of the date ex. 2022-01-01
+        val distinctByDate = txCoinTransferDataList.distinctBy { it.blockTimestamp.take(10) }
+        val uniqueDates = distinctByDate.map { it.blockTimestamp }.toList()
+
+        return uniqueDates.map { date ->
+            val txAmtByDate = txCoinTransferDataList.filter {
+                it.blockTimestamp.take(10) == date.take(10)
+            }.map { it.amount.toLong() }
+
+            TxDailyTotal(
+                address = address,
+                date = date.take(10),
+                total = calcTotalAmt(txAmtByDate),
+                denom = "nhash"
+            )
+        }.toList()
     }
 
     private fun calcTotalAmt(amount: List<Long>): Long {
