61730b1a56617b88aa6ed4db5f6287e8de16c81f
Robert Chaing <rchaing@figure.com>
2022-12-15 12:59:56 -0800

    rever api port

--- src/main/kotlin/tech/figure/aggregate/service/Main.kt
+++ src/main/kotlin/tech/figure/aggregate/service/Main.kt
@@ -193,7 +193,7 @@ fun main(args: Array<String>) {
 
         // start api
         async {
-            embeddedServer(Netty, port=8081) {
+            embeddedServer(Netty, port=8080) {
                 install(Routing) {
                     configureRouting(properties, dwUri, config.dbConfig, config.apiHost)
                 }
