917e5eb34c50f4c3f82a299b7465bfe00fe3b46f
Robert Chaing <rchaing@figure.com>
2023-06-16 14:07:22 -0700

    try this

--- src/main/kotlin/tech/figure/aggregate/service/stream/kafka/BaseKafkaProducer.kt
+++ src/main/kotlin/tech/figure/aggregate/service/stream/kafka/BaseKafkaProducer.kt
@@ -1,7 +1,7 @@
 package tech.figure.aggregate.service.stream.kafka
 
-import tech.figure.aggregate.service.stream.extractors.model.CoinTransfer
-import tech.figure.aggregate.service.stream.extractors.model.MarkerSupply
+import tech.figure.aggregate.common.models.stream.CoinTransfer
+import tech.figure.aggregate.common.models.stream.MarkerSupply
 import tech.figure.aggregate.service.stream.kafka.BaseKafkaProducerParam.CoinTransferParam
 import tech.figure.aggregate.service.stream.kafka.BaseKafkaProducerParam.MarkerSupplyParam
 import tech.figure.aggregate.service.stream.kafka.producer.CoinTxKafkaProducer
@@ -19,7 +19,7 @@ class KafkaProducerFactory(
     private val producers: List<BaseKafkaProducer>
 ) {
 
-    fun publish(param: BaseKafkaProducerParam) = when(param) {
+    suspend fun publish(param: BaseKafkaProducerParam) = when(param) {
         is CoinTransferParam -> producers.filterIsInstance<CoinTxKafkaProducer>().first().publish(param)
         is MarkerSupplyParam -> producers.filterIsInstance<MarkerSupplyKafkaProducer>().first().publish(param)
     }
--- src/main/kotlin/tech/figure/aggregate/service/stream/kafka/producer/MarkerSupplyKafkaProducer.kt
+++ src/main/kotlin/tech/figure/aggregate/service/stream/kafka/producer/MarkerSupplyKafkaProducer.kt
@@ -3,11 +3,16 @@ package tech.figure.aggregate.service.stream.kafka.producer
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import org.apache.kafka.clients.producer.Callback
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.clients.producer.RecordMetadata
 import tech.figure.aggregate.common.logger
 import tech.figure.aggregate.service.stream.kafka.BaseKafkaProducer
 import tech.figure.aggregate.service.stream.kafka.BaseKafkaProducerParam
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
 class MarkerSupplyKafkaProducer(
     private val producerProps: Map<String, Any>,
@@ -16,17 +21,56 @@ class MarkerSupplyKafkaProducer(
 ): BaseKafkaProducer {
 
     private val log = logger()
-    fun publish(param: BaseKafkaProducerParam.MarkerSupplyParam) {
+    suspend fun publish(param: BaseKafkaProducerParam.MarkerSupplyParam) {
         log.info("publishing marker supply data to kafka - BlockHeight: ${param.markerSupplyData.blockHeight}" )
-        runBlocking {
-            producer.send(
-                ProducerRecord(
-                    topicName,
-                    param.markerSupplyData.blockHeight.toString(),
-                    Json.encodeToString(param.markerSupplyData).toByteArray()
-                )
-            )
-        }
+//        runBlocking {
+//            producer.send(
+//                ProducerRecord(
+//                    topicName,
+//                    1,
+//                    param.markerSupplyData.blockHeight.toString(),
+//                    Json.encodeToString(param.markerSupplyData).toByteArray()
+//                )
+//            )
+//        }
+
+
+        val producerRecord = ProducerRecord(
+            topicName,
+            1,
+            param.markerSupplyData.blockHeight.toString(),
+            Json.encodeToString(param.markerSupplyData).toByteArray()
+        )
+
+//        producer.send(producerRecord) { recordMetadata, _ ->
+//            // executes every time a record is successfully sent or an exception is thrown
+//            //if (e == null) {
+//                // the record was successfully sent
+//                println(
+//                    "Received new metadata. \n" +
+//                            "Topic:" + recordMetadata.topic() + "\n" +
+//                            "Partition: " + recordMetadata.partition() + "\n" +
+//                            "Offset: " + recordMetadata.offset() + "\n" +
+//                            "Timestamp: " + recordMetadata.timestamp()
+//                )
+//           // } else {
+//
+//           // }
+//        }
+
+        producer.dispatch(producerRecord)
     }
+
+    suspend inline fun <reified K : Any, reified V : Any> KafkaProducer<K, V>.dispatch(record: ProducerRecord<K, V>) =
+        suspendCoroutine<RecordMetadata> { continuation ->
+            val callback = Callback { metadata, exception ->
+                if (metadata == null) {
+                    continuation.resumeWithException(exception!!)
+                } else {
+                    continuation.resume(metadata)
+                }
+            }
+            this.send(record, callback)
+        }
 
 }
